e8f642b39503beaef2823bf263be1f36d7c45ed9
Nezuko-ski <sheezgatgreatass@gmail.com>
2022-10-05 01:43:19 +0100

    Add project files.

---
+++ ECommerceApp.API/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using ECommerceApp.Core.DTO;
+using ECommerceApp.Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+        private readonly IUnitOfWork _unit;
+
+        public AuthController(IAuthService service, IUnitOfWork unit)
+        {
+            _service = service;
+            _unit = unit;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationDTO dTO)
+        {
+            return StatusCode(200,await _service.RegisterAsync(dTO));
+        }
+        
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO dTO)
+        {
+            try
+            {
+                //HttpContext.User;
+                if(!await _service.LoginAsync(dTO))
+                {
+                    return Unauthorized();
+                }
+               // var user = await _unit.UserRepository.GetAsync(user => user.Email.Equals(dTO.Email.ToLower()));
+                return Accepted(new { Token = await _service.CreateToken()});
+            }
+            catch (Exception ex)
+            {
+                return Problem($"Something went wrong in the {nameof(Login)}, {ex.Message}, {ex.StackTrace}", statusCode: 500);
+            }
+        }
+    }
+}
---
+++ ECommerceApp.Core/Utilities/ECommerceProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ECommerceApp.Core.DTO;
+using ECommerceApp.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Core.Utilities
+{
+    public class ECommerceProfile: Profile
+    {
+        public ECommerceProfile()
+        {
+            CreateMap<User, LoginDTO>();
+            CreateMap<OrderDetail, OrderDetailsDTO>();
+            CreateMap<Product, ProductDTO>();
+            CreateMap<User, RegistrationDTO>().ReverseMap()
+                .ForMember(dest => dest.Email, act => act.MapFrom(src => src.Email.ToLower()));
+        }
+    }
+}
